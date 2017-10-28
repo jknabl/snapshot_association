b72924ee43fbf75af988c9edc5c89d4f46d68d53
Jason Knabl <jason.knabl@gmail.com>
2017-10-28 12:58:32 -0400

    Spacing.

--- lib/snapshot_association/models/snapshot_methods.rb
+++ lib/snapshot_association/models/snapshot_methods.rb
@@ -1,9 +1,9 @@
 module SnapshotAssociation
   module SnapshotMethods
     def snapshot(snapshot_klass_name, column_mapping: {})
-        snapshot_klass = snapshot_klass_name.to_s.classify.constantize
-        s = ::SnapshotAssociation::Snapshot.new(self, snapshot_klass, column_mapping)
-        s.register_callback
-      end
+      snapshot_klass = snapshot_klass_name.to_s.classify.constantize
+      s = ::SnapshotAssociation::Snapshot.new(self, snapshot_klass, column_mapping)
+      s.register_callback
+    end
   end
 end
